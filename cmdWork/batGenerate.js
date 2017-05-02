54f1d036a8287159fcf425d6efd6e766b78b44be
Vintizer <vizerkharkiv@gmail.com>
2017-05-02 17:08:13 +0300

    test generator

--- cmdWork/batGenerate.js
+++ cmdWork/batGenerate.js
@@ -1,2 +1,24 @@
-var fs = require('fs')
-module.exports = () => {};
+var fs = require('fs');
+
+
+module.exports = (dcri, cb) => {
+	var fname = "startDualMiningTest.bat";
+	var innerText = "PING 1.1.1.1 -n 1 -w 3000 >NUL \r\n" +
+		"EthDcrMiner64.exe" +
+		" -epool eu2.ethermine.org:4444 -ewal 0xc907ad492c73af59305a6bac703f87dd5e35c092.sapph -epsw x" +
+		" -dpool dcr.coinmine.pl:2222 -dwal vintizer.vinti -dpsw 774 -allpools 1 -gser 1 -dcri " + dcri;
+	fs.open(fname, "w+", 644, function(err, file_handle) {
+		if (!err) {
+			fs.write(file_handle, innerText, null, 'ascii', function(err, written) {
+				if (!err) {
+					cb();
+					// console.log("Текст успешно записан в файл");
+				} else {
+					// console.log("Произошла ошибка при записи");
+				}
+			});
+		} else {
+			// console.log("Произошла ошибка при открытии");
+		}
+	});
+};
--- server.js
+++ server.js
@@ -1,36 +1,34 @@
 var stopProcess = require("./cmdWork/stopProcess");
 var batGenerate = require("./cmdWork/batGenerate");
-var batStart = require("./cmdWork/batStart");
+var batStart = require("./batStart");
 var getData = require("./get_data/getDataMain");
 var generateReport = require("./report/generateReport");
 const localeConst = require('./const.json');
 
-var request = require('request');
-var dataMiner = require("./get_data/dataMiner");
+
 
 
 
 var commonData = {};
 var mainFunction = (dcri) => {
 	stopProcess();
-	batGenerate(dcri);
-	batStart();
-	setTimeout(function() {
-		getData(function(data) {
-			commonData[dcri] = data;
-			console.log(dcri + " dcri - $" + commonData[dcri]);
-			if (dcri < localeConst.DCRI_STOP) {
-				mainFunction(dcri + 1);
-			} else {
-				generateReport(commonData);
-			}
-		});
+	batGenerate(dcri, function() {
+		batStart();
+		setTimeout(function() {
+			getData(function(data) {
+				commonData[dcri] = data;
+				console.log(dcri + " dcri - $" + commonData[dcri]);
+				if (dcri < localeConst.DCRI_STOP) {
+					mainFunction(dcri + 1);
+				} else {
+					generateReport(commonData);
+				}
+			});
+		}, localeConst.TIMEOUT);
+	});
 
-	}, localeConst.TIMEOUT);
 };
 // mainFunction(localeConst.DCRI_START);
-console.log(process.title);
-
-
+batStart();
 //  https://github.com/badmofo/ethereum-mining-calculator/blob/master/compute.py
 // https://github.com/karldiab/EthereumClassicMiningCalculator/blob/master/js/app1.js
